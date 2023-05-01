eb3525bc95ff7b1121fe0b520fa8c33a1afc600b
Islam Dagaev <spy_002@bk.ru>
2023-05-01 15:06:38 +0500

    сделано домашнее задание 3 семинара

---
+++ zad1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+double DistantLion3d(double ax, double ay, double az, double bx, double by, double bz)
+{
+    double line = Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2) + Math.Pow(az - bz, 2));
+    return Math.Round(line, 2);
+}
+
+Console.Write("Введите A X:");
+double ax = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите A Y:");
+double ay = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите A Z:");
+double az = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите B X:");
+double bx = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите B Y:");
+double by = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите B Z:");
+double bz = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(DistantLion3d(ax, ay, az, bx, by, bz));
---
+++ zad3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+bool PalidromDaNet(int n)
+{
+    int r=0;
+    int nn=n;
+    while (n>0)
+    {
+        r=r*10+n%10;
+        n=n/10;
+    }
+    return r==nn;
+}
+Console.Write("Введите число: ");
+int n=Convert.ToInt32(Console.ReadLine());
+if (PalidromDaNet(n))
+{
+    Console.WriteLine($"{n} -> да");
+}
+else
+{
+    Console.WriteLine($"{n} -> нет");
+}   
+    
